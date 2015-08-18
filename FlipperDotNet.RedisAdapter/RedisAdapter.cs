03c11d8eae077ab62882dc1bbabf5de19d47d5c0
Graham Davison <g.m.davison@computer.org>
2015-08-18 00:19:54 -0700

    Enables and disables Boolean gates

--- FlipperDotNet.RedisAdapter/RedisAdapter.cs
+++ FlipperDotNet.RedisAdapter/RedisAdapter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using FlipperDotNet.Adapter;
 using FlipperDotNet.Gate;
 using StackExchange.Redis;
@@ -19,15 +20,30 @@ namespace FlipperDotNet.RedisAdapter
 
 		public IDictionary<string, object> Get(Feature feature)
 		{
-			return new Dictionary<string,object>();
+			var result = new Dictionary<string, object>();
+
+			var values = _database.HashGetAll(feature.Key);
+
+			foreach (var gate in feature.Gates)
+			{
+				if (gate.DataType == typeof(bool))
+				{
+					var value = values.SingleOrDefault(x => x.Name.ToString() == gate.Key);
+					result[gate.Key] = value.Value.ToString();
+				}
+			}
+
+			return result;
 		}
 
 		public void Enable(Feature feature, IGate gate, object thing)
 		{
+			_database.HashSet(feature.Key, gate.Key, thing.ToString().ToLower());
 		}
 
 		public void Disable(Feature feature, IGate gate, object thing)
 		{
+			_database.KeyDelete(feature.Key);
 		}
 
 		public ISet<string> Features {
