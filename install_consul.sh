394405024162633519c52a63535c6edc06fc38fe
Graham Davison <gdavison@paybyphone.com>
2015-09-16 14:18:01 -0700

    Refactored sudo commands from install_consul script

--- install_consul.sh
+++ install_consul.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install unzip
-
 wget 'https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip' -O consul.zip
 unzip consul.zip
-sudo chmod +x consul
-sudo mv consul /usr/bin/consul
+chmod +x consul
 
-consul agent -server -client 0.0.0.0 -bootstrap-expect 1 -data-dir /tmp/consul &
+./consul agent -server -client 0.0.0.0 -bootstrap-expect 1 -data-dir /tmp/consul &
